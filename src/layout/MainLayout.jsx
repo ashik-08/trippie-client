c351bd8567185f9102fd95c244552845686c14e1
ashik-08 <ashikurrahman.8october@gmail.com>
2024-05-07 06:04:41 +0600

    [feat](footer): installed material-tailwind and added Footer to MainLayout

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,9 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer/Footer";
 import NavBar from "../components/NavBar/NavBar";
 
 const MainLayout = () => {
   return (
     <section>
       <NavBar />
+      <div className="min-h-[calc(100vh-522px)]">
+        <Outlet />
+      </div>
+      <Footer />
     </section>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
       volkhov: ["Volkhov", "serif"],
@@ -12,10 +13,10 @@ export default {
         title: "#222222",
         paragraph: "#666666",
         back: "#C1C1C1",
-        'sub-head': "#333333",
+        "sub-head": "#333333",
         details: "#888888",
       },
     },
   },
   plugins: [require("daisyui")],
-};
+});
